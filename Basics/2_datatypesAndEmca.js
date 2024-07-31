befc5e42545dfd6be843084c41adb469a4328e1c
AkashYadav000 <akashyadavsihma@gmail.com>
2024-07-31 11:50:40 +0530

    2_datatypesAndEmca

---
+++ Basics/2_datatypesAndEmca.js
@@ -0,0 +1,20 @@
+"use Strict"; //treat all js code as newer version
+
+// alert(3+3)// we are using nodejs,not browser
+
+console.log(6+6);
+
+console.log("Akash");
+
+//primitive datatypes
+//string
+//number
+//boolean
+//null
+//undefined
+//symbol
+let name="yadav"
+let age=20
+let isLoggedIn=true
+let state=null;
+console.log(typeof isLoggedIn)
