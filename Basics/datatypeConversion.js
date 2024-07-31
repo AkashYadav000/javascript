2b1c5719e996777a2fbf08acd42119f8fb68db97
AkashYadav000 <akashyadavsihma@gmail.com>
2024-07-31 12:03:42 +0530

    conversion of datatypes

---
+++ Basics/datatypeConversion.js
@@ -0,0 +1,19 @@
+let score="77"
+// console.log(typeof score)
+
+let valueInNumber =Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// 33->33
+//abc->NaN (Not a Number)
+// true->1 // false-> 0;
+
+let isLoggedIn=0;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
