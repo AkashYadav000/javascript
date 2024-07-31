abdf1c339f1227cbe3308f546d89bad0667e16d2
AkashYadav000 <akashyadavsihma@gmail.com>
2024-07-31 12:22:12 +0530

    datatypeConversion

--- Basics/datatypeConversion.js
+++ Basics/datatypeConversion.js
@@ -15,5 +15,27 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************************* Operrations ******************
+
+let value=3
+let negValue=-(value)
+// console.log(negValue)
+// console.log(negValue*negValue)
+
+let str1="Hello"
+let str2=" Akash"
+let str3=" Yadav"
+let str4=str1+str2+str3
+
+// console.log(str4)
+// console.log("1"+2+2)
+// console.log(1+"2"+2)
+// console.log(1+2+"2")
+
+
+let gameCounter=78
+gameCounter++
+console.log(gameCounter)
