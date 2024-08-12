2fcc9d2cbd5899527a187cca177082d91303037d
AkashYadav000 <akashyadavsihma@gmail.com>
2024-08-12 23:00:52 +0530

    2_functions

---
+++ Bascis_3/2_functions.js
@@ -0,0 +1,22 @@
+function print(newArray){
+return newArray;
+}
+
+// console.log(print([1,11.2,232,3545,4546,532,654,754]));
+
+
+const user={
+    username:"Akash",
+    age:23,
+}
+function namee(object) {
+  console.log(`this is ${object.username} and my age is ${object.age}`)
+}
+
+// namee(user);
+
+
+function calculate(...num1){
+    return num1;
+}
+console.log(calculate(200,33,322,2353,534,423));
