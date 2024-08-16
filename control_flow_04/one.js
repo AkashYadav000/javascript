baaecaf9326a25fa3ea53ad4bd38c6e14cb8b836
AkashYadav000 <akashyadavsihma@gmail.com>
2024-08-16 16:02:18 +0530

    onejs

---
+++ control_flow_04/one.js
@@ -0,0 +1,54 @@
+
+const temp=69;
+
+// if(2=="2"){
+//     console.log("chal gya bhyi");    
+// }
+
+
+
+// if(temp>43){
+//     console.log("g faad temp hai bhai");  
+// }else{
+//     console.log("temp kam hai");   
+// }
+
+
+
+// const price="9000"
+// if(price>800)console.log("waah yaar cheete");//not recommended
+
+
+
+// const balance=770
+// if(balance<900&&balance>800){
+//     console.log("less than 900 and greater than 800");
+    
+// }else if(balance<800&&balance>700){
+//     console.log("less than 800 and greater than 700");
+    
+// }else if(balance<700&&balance>600){
+//     console.log("less than 700 and greater than 600");
+    
+// }else if(balance<600&&balance>500){
+//     console.log("less than 600 and greater than 500");
+    
+// }else{
+//     console.log("balance is more than");
+    
+// }
+
+
+const userloggedin=true;
+const debitCard=true;
+const loggedInFromGoogle=true;
+const loggedInFromEmail=false;
+
+// if(userloggedin&&debitCard&&2==2){
+//     console.log("paise udaao bhencho");
+// }
+
+if(loggedInFromGoogle||loggedInFromEmail){
+    console.log("hogya login");
+    
+}
